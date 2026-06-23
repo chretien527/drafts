2710348ba794a0bc0370b7c9932fe8b6e81e7ac8
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-06-23 21:39:57 +0200

    Add files via upload

--- create.php
+++ create.php
@@ -1,31 +1,23 @@
 <?php
-
-include 'Connections.php';
-if(isset($POST['submit'])){
-    $first_name = $_POST['firstname'];
-    $last_name = $_POST['lastname'];
+include 'connections.php';
+if(isset($_POST['submit'])){
+    $firstname = $_POST['fname'];
+    $lastname = $_POST['lname'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
     $gender = $_POST['gender'];
-
-    $sql = "INSERT INTO users(firstname, lastname,email,gender,password)
-    Values('$first_name','$last_name','$email','$password','$gender')";
-
+    $password = sha1($_POST['password']);
+    $sql = "INSERT INTO users(fname,lname,email,gender,password) VALUES 
+    ('$firstname','$lastname','$email','$gender','$password')";
     $result = $conn->query($sql);
-
-    if($result == true){
-        echo "New record created successfully.";
+    if($result === true){
+        echo("Record added successfully.");
     } else {
         echo "Error".$sql."<br>".$conn->error;
     }
     $conn->close();
 }
-
 ?>
-
 <html>
-
-<a class="btn btn info" href="signup.html"><br><br>Back</a>
-<a class="btn btn info" href="read.php"><br><br>View record from database</a>
-
+<a href='form.html'><br><br>Go back</a>
+<a href='read.php'><br><br>View data records<a/>
 </html>
