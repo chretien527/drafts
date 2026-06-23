e5ea4aad6ebef5302528e66e282c0cfff28a8bdf
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-06-23 21:39:36 +0200

    Add files via upload

--- connections.php
+++ connections.php
@@ -1,28 +1,13 @@
 <?php
-$username = 'root';
 $servername = 'localhost';
-$password = '2010';
-$dbname = 'y1b_db';
-
-$connection = mysqli_connect($servername,$username,$password,$dbname);
-
-if(!$connection){
-    alert("Connection failed:".mysqli_connect_error);
-}
-echo("Connection succeded");
-mysqli_close($connection);
-?>
-<?php
 $username = 'root';
-$servername = 'localhost';
 $password = '2010';
-$dbname = 'y1b_db';
 
-$connection = new mysqli($servername.$username,$password,$dbname);
-
-if($connection->connect_error){
-    alert("Connection failed:".$connection->connect_error);
-} else {
-    echo("Connection succeded.");
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=y1b_db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
